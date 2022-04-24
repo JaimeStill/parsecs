e7480e94f357e9659c4e5961e763974ba883037d
Jaime Still <jpstill85@gmail.com>
2022-04-23 22:12:01 -0400

    initial crew feature added

--- src/core/src/models/character.ts
+++ src/core/src/models/character.ts
@@ -1,9 +1,11 @@
 import { Race } from './race';
 
 export class Character<T extends Race> {
+  readonly id: Symbol;
   readonly race: T;
 
   constructor(race: T) {
+    this.id = Symbol();
     this.race = race;
   }
 }
---
+++ src/core/src/models/crew.ts
@@ -0,0 +1,28 @@
+import { Character } from './character';
+import { Race } from './race';
+
+export class Crew {
+  private _leader: Character<Race> | null  = null;
+  private _roster: Character<Race>[];
+
+  constructor() {
+    this._roster = new Array<Character<Race>>();
+  }
+
+  get leader(): Character<Race> | null { return this._leader; }
+
+  set leader(crew: Character<Race> | null) {
+    this.leader && crew
+      ? this.leader
+      : crew;
+  }
+
+  get roster() { return this._roster; }
+
+  addCharacter = (character: Character<Race>) =>
+    !(this.roster.some(c => c.id === character.id))
+      && this.roster.push(character);
+
+  removeCharacter = (character: Character<Race>) =>
+    this._roster = this.roster.filter(c => !(c.id === character.id));
+}
